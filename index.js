8e635386ad147e6fc5b3063074ed65ce8ee037f5
Sushil Kumar <sushil@Sushils-MacBook-Air.local>
2024-02-03 10:10:33 +0530

    v4

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var i = 0;
-//var message = 
-//"Hi Monika.....! << I want to say something special to you. <<< So, please read it carefully...! > I am glad we came to know each other. < You seemed something special to me. << As the days go by < you get closer to me....! << I don't know the reason why your thoughts always resonate inside my mind...! > Everything about you is always interesting to me...! << fsfsfsf > fjisjf....! << fasfsfs....! > I like you, Sweety.....! | <<<< kjisjfl";
 
 var message=
-"Hi Monika....!!   << I want to say something special to you.<< So, please read it carefully...!  > I feel fortunate that our paths crossed, <and we got to know each other.  <<I am truly grateful for the time we  spent <getting to know each other over these past few days. <<You have  become  someone special to me and <as the days go by , you get closer to me.!   <<<Your presence brightens my days.<<<Thanks for coming in my life....|<< Sweety!! ";
+"Hi Monika....!!   << I want to say something special to you.<< So, please read it carefully...!  > I feel fortunate that our paths crossed, <and we got to know each other.  <<I am truly grateful for the time we  spent <getting to know each other over these past days. <<You have  become  someone special to me and <as the days go by , you get closer to me.!   <<<Your presence brightens my days.<<<Thanks for coming in my life....|<< Sweety!! ";
 
 var typingSpeed = 90; // Decreased typing speed
 typeWriter();
